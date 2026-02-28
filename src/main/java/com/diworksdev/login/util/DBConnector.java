7ca5cb579c7603ed09a60e25e7eb34cceba292b8
saho <jyako.t@gmail.com>
2026-02-28 23:11:14 +0900

    driverクラス名変更

--- src/main/java/com/diworksdev/login/util/DBConnector.java
+++ src/main/java/com/diworksdev/login/util/DBConnector.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-	private static String driverName = "com.mysql.jdbc.Driver";
+	private static String driverName = "com.mysql.cj.jdbc.Driver";
 	private static String url = "jdbc:mysql://localhost/logindb_daibo";
 	
 	private static String user = "root";
